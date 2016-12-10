f691f1e935424474f5001395f72120d75ff9e4bb
Remus Victuelles <remus.victuelles@gmail.com>
2016-12-10 20:17:25 +1100

    My first commit.

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tapCounter = 0;
+    @IBOutlet weak var lblHey: UILabel!
+    
+    @IBAction func btnTapped(_ sender: Any) {
+        /* Change label text as button is tapped
+         */
+        lblHey.text = "Hey World!";
+        
+        
+        if (tapCounter >= 10) {
+            lblHey.text = "Hey, too much tapping!";
+            
+        } else {
+            tapCounter += 1;
+            print("btnTapped activated"); // Log button tap
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
